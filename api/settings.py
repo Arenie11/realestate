d7615824b7fda8e2493a0bd26fc2a8273c731a19
Zainab Alugo <zainabalugo@gmail.com>
2025-02-03 01:46:57 +0100

    settings

--- api/settings.py
+++ api/settings.py
@@ -79,22 +79,22 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'realestate',
-        'USER': 'postgres',
-        'PASSWORD': 'Tytz8611',  # Ensure this is correct!
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-}  
+}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'realestate',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Tytz8611',  # Ensure this is correct!
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }  
 
 
 # Password validation
