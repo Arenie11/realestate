0f45e1a53d22fa31482ee65c7f2c90a155cc7580
Zainab Alugo <zainabalugo@gmail.com>
2025-02-03 03:24:58 +0100

    allowed host

--- api/settings.py
+++ api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = str( os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://realestate-yqmi.onrender.com' ,'localhost:8000', '127.0.0.1']
+ALLOWED_HOSTS = ['http://realestate-yqmi.onrender.com','https://realestate-yqmi.onrender.com' ,'localhost:8000', '127.0.0.1']
 
 
 # Application definition
