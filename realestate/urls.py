215b2d0de8e64dddf7fd205a8469baaa2186b7ae
Zainab Alugo <zainabalugo@gmail.com>
2025-02-05 00:41:31 +0100

    corrections in getlisting

--- realestate/urls.py
+++ realestate/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns= [
 
     path('listings/', views.ListingView.as_view()),
-    path('getlisting/', views.GetListingView.as_view()),
+    path('getlisting/<int:pk>/', views.GetListingView.as_view()),
     path('inquiry/', views.InquiryView.as_view()),
     path('SearchListings/', views.SearchListingsView.as_view()),
     path('payment/<str:pk>', views.PaymentPageView.as_view(),name='payment'),
