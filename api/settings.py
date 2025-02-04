28730ba97f94b45eadbf8e6d405a6a7260ca5440
Zainab Alugo <zainabalugo@gmail.com>
2025-02-04 12:32:59 +0100

    correction

--- api/settings.py
+++ api/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str( os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['realestate-yqmi.onrender.com','https://realestate-yqmi.onrender.com' ,'localhost:8000', '127.0.0.1']
 
